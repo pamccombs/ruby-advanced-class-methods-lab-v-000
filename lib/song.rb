4c788d86513fda482808d6eba82d92f20342f224
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-30 14:51:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,11 +56,9 @@ class Song
   end
   
   def self.create_from_filename(filename)
-    row = filename.split(" - ")
-    artist_name = row[0]
-    song_name = row[1].sub(/.mp3/, '')
     
-    song = self.new
+    
+    song = 
     song.save
     
   end
